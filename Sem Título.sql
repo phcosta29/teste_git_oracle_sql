5c7faf87a1fc98ac5fa10c43c9485ad7e9d9bbdc
phcosta29 <ph.oliveiracosta@gmail.com>
2017-10-26 13:00:49 -0200

    Update Sem Título.sql

--- Sem Título.sql
+++ Sem Título.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Teste2  
    (ProductID int PRIMARY KEY NOT NULL,  
-    ProductName VARCHAR2(25) NOT NULL,  
+    ProductName VARCHAR2(20) NOT NULL,  
     Price FLOAT NOT NULL,  
-    ProductDescription VARCHAR2(100) NOT NULL);
+    ProductDescription VARCHAR2(150) NOT NULL);
