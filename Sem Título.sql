a493c4ec06858dd04975f4274198d6aa4fca86ef
costaped <costaped@WBRGUABX3KJ82.Mars-AD.Net>
2017-10-26 12:07:38 -0200

    teste

---
+++ Sem Título.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Teste2  
+   (ProductID int PRIMARY KEY NOT NULL,  
+    ProductName VARCHAR2(25) NOT NULL,  
+    Price FLOAT NOT NULL,  
+    ProductDescription VARCHAR2(100) NOT NULL);
